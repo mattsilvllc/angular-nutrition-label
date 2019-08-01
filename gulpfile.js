a8a7fac3d68f67c6968b77d61b9495d84a8d4311
Yurko Fedoriv <yurko.fedoriv@syndigo.com>
2019-08-01 15:59:56 +0300

    upgraded libs and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const argv = require('yargs').argv;
+const argv = require('yargs').version(false).argv;
 const gulp = require('gulp');
 gulp.plugins = require('gulp-load-plugins')();
-const gutil = require('gulp-util');
 
 const fs = require('fs-extra');
 
@@ -11,17 +10,17 @@ const bowerJson = require(__dirname + '/bower.json');
 const name = bowerJson['name'];
 const libFile = `./src/${name}.js`;
 
-gulp.task('clean', function () {
+gulp.task('clean', function (done) {
   fs.removeSync(`./${name}.js`);
   fs.removeSync(`./${name}.min.js`);
+  done();
 });
 
-gulp.task('setVersion', function () {
+gulp.task('setVersion', function (done) {
   let version = argv.version;
+
   if (!version) {
-    console.error('--version=x.x.x param is required');
-    process.exit(1);
-    return;
+    throw new Error('--version=x.x.x param is required');
   }
 
   ['bower.json', 'package.json'].forEach(file => {
@@ -40,31 +39,31 @@ gulp.task('setVersion', function () {
       .toString()
       .replace(/@version\s+[^\s\n]+/, `@version ${version}`)
   );
+
+  done();
 });
 
-gulp.task('build.es5.js', ['clean'], function () {
+gulp.task('build.es5.js', gulp.series('clean', function () {
   return gulp.src(`./src/${name}.js`)
-    .pipe(gulp.plugins.babel({presets: ['es2015']}))
+    .pipe(gulp.plugins.babel({presets: ['@babel/preset-env']}))
     .pipe(gulp.plugins.ngAnnotate())
     .pipe(gulp.dest(__dirname + '/'));
-});
+}));
 
-gulp.task('build.min.js', ['build.es5.js'], function () {
+gulp.task('build.min.js', gulp.series('build.es5.js', function () {
   return gulp.src(`./${name}.js`)
-    .pipe(gulp.plugins.stripNgLog())
+    .pipe(gulp.plugins.replace(/[ \t]*\$log\..*?[\n;]/g, ''))
     .pipe(gulp.plugins.rename(`${name}.min.js`))
     .pipe(gulp.plugins.uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+    .on('error', (err) => console.error('[Error]', err.toString()))
     .pipe(gulp.dest('./'));
-});
+}));
 
 
-gulp.task('default', ['build.min.js'], function () {
-
-});
+gulp.task('default', gulp.series('build.min.js', (done) => done()));
 
 
 //TODO: tests
-gulp.task('test', function () {
-
+gulp.task('test', function (done) {
+  done();
 });
